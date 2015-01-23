54c95fb4ee5d25a40b32e6521001d62abb08a76a
Pablo Cabeza <lemniscata.lmn@gmail.com>
2015-01-23 14:41:15 +0100

    profile info

--- _cactus/static/js/main.js
+++ _cactus/static/js/main.js
@@ -6,23 +6,23 @@
 
   - drender.getvar(dot-string) devuelve el valor de la variable
   - drender.setver(dot-string,valor) pone la variable al valor y
-  				     actualiza los elementos
+                                     actualiza los elementos
 
   - en /underscore se añaden de templates, su nombre (sin extensión)
     se usa como el id del template
 */
-var drender = (function(doc,win,$){ 
+var drender = (function(doc,win,$){
 
     // configure underscore templates
     _.templateSettings = {
-	interpolate: /\{\{(.+?)\}\}/g,
-	evaluate: /\{%([\s\S]+?)%\}/g
+        interpolate: /\{\{(.+?)\}\}/g,
+        evaluate: /\{%([\s\S]+?)%\}/g
     };
 
     var ret = {}
     var templates;
     var DATA_J = "APP_DATA" in win.localStorage ? win.localStorage.APP_DATA : null,
-    	DATA = DATA_J ? JSON.parse(DATA_J) : win.APP_DATA;
+        DATA = DATA_J ? JSON.parse(DATA_J) : win.APP_DATA;
 
 
     /**
@@ -32,78 +32,84 @@ var drender = (function(doc,win,$){
        - prop1[{dot-string}] --> uses dot-string as an index (fetch its value first) for prop1
      */
     function byString(o, s) {
-	// transfor, {var} to its var, for example array[{var}] to access array indirectly
-	s = s.replace(/\{([^}]+)\}/g, function(match, gr1, offset, string) {
-	    return byString(o,gr1)[0];
-	});
+        // transfor, {var} to its var, for example array[{var}] to access array indirectly
+        s = s.replace(/\{([^}]+)\}/g, function(match, gr1, offset, string) {
+            return byString(o,gr1)[0];
+        });
 
-	s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
-	s = s.replace(/^\./, '');           // strip a leading dot
-	var a = s.split('.'), prev_o = null, prev_idx= null;
-	while (a.length) {
-    	    var n = a.shift();
-    	    if (n in o) {
-		prev_o = o;
-		prev_idx = n;
-    		o = o[n];
-    	    } else {
-    		return;
-    	    }
-	}
-	return [o,prev_o,prev_idx];
+        s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
+        s = s.replace(/^\./, '');           // strip a leading dot
+        var a = s.split('.'), prev_o = null, prev_idx= null;
+        while (a.length) {
+            var n = a.shift();
+            if (n in o) {
+                prev_o = o;
+                prev_idx = n;
+                o = o[n];
+            } else {
+                return;
+            }
+        }
+        return [o,prev_o,prev_idx];
     }
-    
+
 
     // Data getters and setters for global app data
-    ret.getvar = function(s){ return byString(DATA,s)[0]; }
+    ret.getvar = function(s){ console.log(s); return byString(DATA,s)[0]; }
     ret.setvar = function(s,v) {
-	var vars = byString(DATA,s);
-	
-	vars[1][vars[2]] = v; // update actual value of 
-	
-	// update localStorage, it is only text, so first json it
-	DATA_J = JSON.stringify(DATA);
-	win.localStorage.APP_DATA = DATA_J;
+        var c = _.isFunction(v) ? v : function() { return v; }
+        var vars = byString(DATA,s);
+
+        vars[1][vars[2]] = c(vars[0]); // update actual value of
+
+        // update localStorage, it is only text, so first json it
+        DATA_J = JSON.stringify(DATA);
+        win.localStorage.APP_DATA = DATA_J;
 
 
-	// rerender any element with the variable
-	ret.drender("[data-var='"+s+"']");
+        // rerender any element with the variable
+        ret.drender("[data-var='"+s+"']");
 
-	return ret;
+        return ret;
     }
 
 
-    
+    /** update templates var */
     ret.repopulate_templates = function() {
-	templates = $("script[type='text/template']");
+        templates = $("script[type='text/template']");
     }
-    
+
     /** renders a template (given by its id) using variable data **/
     ret.render_template = function(template,variable) {
-	var template = templates.filter('#'+template).eq(0).text();
-	return _.template(template)({"data":ret.getvar(variable)});
+        var template = templates.filter('#'+template).eq(0).text();
+        return _.template(template)({"data":ret.getvar(variable)});
     }
 
-    
+
 
     /** renders all templates */
     ret.drender_all = function() {
-	$('.dynamic-render,.drender').each(function(){ ret.drender(this); });
+        $('.dynamic-render,.drender').each(function(){ ret.drender(this); });
     }
 
     /** renders an element that can be passed to jquery factory */
     ret.drender = function(el) { el = $(el); if(!el.length) return;
-	var template = el.data('template'), variable =el.data('var');
+        var template = el.data('template'), variable =el.data('var');
 
-	if(template) el.html( ret.render_template(template,variable) );
-	else el.html( ret.getvar(variable) );
+        if(template) el.html( ret.render_template(template,variable) );
+        else el.html( ret.getvar(variable) );
     }
-    /** update templates var */
+
+    ret.drender_if_possible = function(el) {
+	el = $(el);
+	if(el.hasClass('dynamic-render') || el.hasClass('drender')) 
+	    return ret.drender(el);
+    }
 
 
     $(function(){
-	ret.repopulate_templates();
-	ret.drender_all();
+        ret.repopulate_templates();
+        ret.drender_all();
     });
 
     return ret;
@@ -116,13 +122,13 @@ $(function(){
         var date = $("#" + id).data("date");
         var hasEvent = $("#" + id).data("hasEvent");
         console.log('You clicked on date ' + date);
-	return true;
+        return true;
     }
 
     $("#app-calendar").zabuto_calendar({
-	language: "es", 
-	data: APP_DATA.notifications.calendar,
-	action: function() { return myDateFunction(this.id); }
+        language: "es",
+        data: APP_DATA.notifications.calendar,
+        action: function() { return myDateFunction(this.id); }
     });
 })
 
@@ -133,12 +139,20 @@ $(function(){
 
     // downcount
     $('.countdown').each(function(){
-	var date=$(this).data('date').split(' ');
-	date[0] = date[0].substr(3, 2)+"/"+date[0].substr(0, 2)+"/"+date[0].substr(6, 4);
+        var date=$(this).data('date').split(' ');
+        date[0] = date[0].substr(3, 2)+"/"+date[0].substr(0, 2)+"/"+date[0].substr(6, 4);
 
-	console.log(date);
-	$(this).downCount({date: date.join(' '), offset: +1});
+        console.log(date);
+        $(this).downCount({date: date.join(' '), offset: +1});
+    });
+
+    
+    // editable setup
+    $('.editable').editable({
+	mode: 'inline',
+	success: function(resp,val) { 
+	    var v = $(this).data('var');
+	    drender.setvar(v,val);
+	}
     });
 });
-
-
